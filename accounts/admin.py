a5c8e3846cd5838cadc74b51afb99e504344c2d8
Suryansh Barnawal <suryanshbarnawal890@gmail.com>
2026-04-30 19:08:52 +0530

    register UserProfile and EmailVerificationToken in admin and update state field choices for destination and tour models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,18 +1,5 @@
 from django.contrib import admin
 from .models import UserProfile, EmailVerificationToken
 
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'phone', 'city', 'email_verified', 'created_at')
-    list_filter = ('email_verified',)
-    search_fields = ('user__username', 'user__email', 'phone', 'city')
-    list_editable = ('email_verified',)
-    readonly_fields = ('created_at',)
-
-
-@admin.register(EmailVerificationToken)
-class EmailVerificationTokenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'token', 'is_used', 'created_at')
-    list_filter = ('is_used',)
-    readonly_fields = ('token', 'created_at')
+admin.site.register(UserProfile)
+admin.site.register(EmailVerificationToken)
